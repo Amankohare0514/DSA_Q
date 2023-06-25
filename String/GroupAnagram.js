cc1a834125797d53e4102b1c15ab3efb9d831060
Aman kohare <amankohare@gmail.com>
2023-06-25 08:44:09 +0530

    morning

---
+++ String/GroupAnagram.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    let obj={};
+    for(let str of strs){
+        let letters=str.split("").sort().join("");
+        obj[letters]? obj[letters].push(str) : obj[letters]=[str];
+    }
+    return Object.values(obj)
+}
