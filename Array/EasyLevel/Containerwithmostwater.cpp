0ed4f93da348e279d910c2db6f787fb7287e9caa
Aman kohare <amankohare@gmail.com>
2023-07-07 18:02:21 +0530

    newj

--- Array/EasyLevel/Containerwithmostwater.cpp
+++ Array/EasyLevel/Containerwithmostwater.cpp
@@ -1,20 +1,20 @@
-class Solution {
-public:
-    int maxArea(vector<int>& height) {
-   int n = height.size();
-   int left = 0 , right=n-1;
-   int maxArea = 0;
-   while(left < right){
-   int area = min(height[left] , height[right]) * (right-left);
-   maxArea = max (maxArea , area);
-   if(height[left] < height[right]){
-    left++;
-   }
-   else{
-  right--;
-   }
-   }
-   return maxArea;
-   }
-    };
+var maxArea = function (height) {
+  let max = 0;
+  let left = 0;
+  let right = height.length - 1;
+
+  while (left < right) {
+    // logic
+    const area = Math.min(height[left], height[right]) * (right - left);
+    max = Math.max(max, area);
+
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return max;
+};
+
     
