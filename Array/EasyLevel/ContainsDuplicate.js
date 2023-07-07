d15c1795f7efde4db348b25a7d4f9dafcfa5be98
Aman kohare <amankohare@gmail.com>
2023-07-07 15:10:47 +0530

    nice

--- Array/EasyLevel/ContainsDuplicate.js
+++ Array/EasyLevel/ContainsDuplicate.js
@@ -3,12 +3,11 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    nums=nums.sort()
-    for (let i = 0; i < nums.length; i++) {
-       if (nums[i] == nums[i+1]) {
-        return true;
-       }
-       return false;
+    nums = nums.sort();
+    for(let i=0; i<nums.length-1; i++){
+        if(nums[i] == nums[i+1]){
+            return true;
+        }
     }
-}
-console.log();
+            return false;
+};
