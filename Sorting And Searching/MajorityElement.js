a8ac8018ec74b67cee667e76485608b7953e5bc9
Aman kohare <amankohare@gmail.com>
2023-07-08 18:12:54 +0530

    nice

--- Sorting And Searching/MajorityElement.js
+++ Sorting And Searching/MajorityElement.js
@@ -1,14 +1,18 @@
 var majority = function (nums, size) {
-    const Map = {};
-    const majorityThreshold = Math.floor(size / 2);
-  
-    for (let i = 0; i < nums.length; i++) {
-      const number = nums[i];
-      Map[number] = (Map[number] || 0) + 1;
-      if (Map[number] > majorityThreshold) {
-        return number;
-      }
-    }
-    return -1;
-  };
+let sol= 0;
+let cnt=0 ;
+for(let i=0; i<nums.length ; i++){
+  if (sol == 0) {
+    sol = nums[i];
+    cnt = 1;
+  }
+  else if (sol == nums[i]) {
+   cnt++
+  }
+  else {
+    cnt--;
+  }
+}
+return sol;
+}
   console.log(majority[3,2,3]);
