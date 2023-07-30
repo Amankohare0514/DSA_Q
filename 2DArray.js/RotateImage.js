97f64ea8210ed877f87aca743a998876cf0f0416
Aman kohare <amankohare@gmail.com>
2023-07-30 23:08:35 +0530

    after

--- 2DArray.js/RotateImage.js
+++ 2DArray.js/RotateImage.js
@@ -1,12 +1,37 @@
-var rotate = function(matrix) {
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+// var setZeroes = function(matrix) {
+//     const rowSet = new Set(),
+//     colSet = new Set()
+//     for(let i= 0 ; i<matrix.length; i++){
+//         for(let j=0; j<matrix[i].length; j++){
+//             if (!matrix[i][j]) {
+//                 rowSet.add(i);
+//                 colSet.add(j);
+//             }
+//         }
+//     }
+//     for(let i= 0 ; i<matrix.length; i++){
+//         for(let j=0; j<matrix[i].length; j++){
+//        if(rowSet.has(i) || colSet.has(j)) matrix[i][j] = 0 ;    
+//       }
+//     }
+//     }
+var setZeroes = function(matrix) {
+    const rowSet = new Set() , colSet = new Set()
     for(let i=0; i<matrix.length ; i++){
-        for(let j=i+1; j<matrix.length ; j++){
-            let temp  = matrix[i][j];
-            matrix[i][j] = matrix[j][i];
-            matrix[j][i] = temp
+        for(let j=0; j<matrix[i].length ; j++){
+            if(!matrix[i][j]){
+                rowSet.add(i);
+                colSet.add(j);
+            }
         }
     }
-    for(let i=0; i<matrix.length ; i++){
-        matrix[i].reverse()
+    for(let i=0; i<matrix.length; i++){
+        for(let j=0; j<matrix[i].length;j++){
+            if(rowSet.has(i) || colSet.has(j)) matrix[i][j]= 0;
+        }
     }
 }
