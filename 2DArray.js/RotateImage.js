69157ecb4eff800fdec862e9610f45a134507140
Aman kohare <amankohare@gmail.com>
2023-07-30 22:56:34 +0530

    after long time

--- 2DArray.js/RotateImage.js
+++ 2DArray.js/RotateImage.js
@@ -1,12 +1,12 @@
 var rotate = function(matrix) {
-    for(let i=0;  i<matrix.length; i++){
-        for(let j=i+1; j<matrix[0].length; j++){
-            temp = matrix[i][j];
-            matrix[i][j]=matrix[j][i];
-            matrix[j][i]=temp;
+    for(let i=0; i<matrix.length ; i++){
+        for(let j=i+1; j<matrix.length ; j++){
+            let temp  = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp
         }
     }
-    for(let i=0;  i<matrix.length; i++){
-        matrix[i].reverse();
+    for(let i=0; i<matrix.length ; i++){
+        matrix[i].reverse()
     }
 }
