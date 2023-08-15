563c52d2ad264bdd5b6de3478428f67141a3051a
Aman kohare <amankohare@gmail.com>
2023-08-15 18:28:34 +0530

    new

---
+++ Array/EasyLevel/Containerwithmostwater.js
@@ -0,0 +1,16 @@
+var maxArea = function (height) {
+ let max = 0 ;
+ let left = 0;
+ let right = height.length -1 ;
+ while(left < right) {
+     const area = Math.min(height[left] , height[right]) * (right-left);
+     max = Math.max(max, area)
+     if(height[left] < height[right]){
+         left++
+     }
+     else {
+         right--
+     }
+ }
+ return max
+}
