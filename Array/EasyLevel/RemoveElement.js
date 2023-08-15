efc0c7e9c0473f9a8eff38f2ecacaccfcde9b538
Aman kohare <amankohare@gmail.com>
2023-08-15 18:42:35 +0530

    nice

--- Array/EasyLevel/RemoveElement.js
+++ Array/EasyLevel/RemoveElement.js
@@ -1,9 +1,9 @@
-var removeElement = function(nums, val) {
+var removeElement = function (nums , val){
     for(let i=0; i<nums.length; i++){
         if(nums[i] == val){
             nums.splice(i,1);
-            i--;
+            i--
         }
     }
-    return nums.length;
-};
+    return nums.length
+}
