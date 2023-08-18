6c05cfa5df4eb3a4540290a1ce6365a83b1dde85
Aman kohare <amankohare@gmail.com>
2023-08-18 09:39:39 +0530

    nice

--- LinkedList/IsPalindrome.js
+++ LinkedList/IsPalindrome.js
@@ -1,18 +1,18 @@
-let isPalindrome = function (head) {
+var isPalindrome = function(head){
     let values = [];
     let current = head;
-    while(current !== null){
-        values.push(current.next)
-        current = current.next;
+    while(current != null){
+        values.push(current.val)
+        current = current.next
     }
     let left = 0;
-    let right = values.length - 1;
-    while(left < right) {
-        if (values[left] !== values[right]) {
+    let right = values.length -1;
+    while(left < right){
+        if(values[left] !== values[right]){
             return false
         }
-        left ++ ;
-        right  --;
+        left ++;
+        right --;
     }
-    return false;
+    return true
 }
