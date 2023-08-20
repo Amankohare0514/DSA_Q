94d37069ee945adf01a9f616e2d93bcd30f46658
Aman kohare <amankohare@gmail.com>
2023-08-20 10:11:58 +0530

    node

---
+++ Tree/rightSideview.js
@@ -0,0 +1,13 @@
+var rightSideView = function(root) {
+    let result = [];
+    function traverse(node , level){
+        if(!node) return ;
+        if(result.length === level){
+            result.push(node.val)
+        }
+        traverse(node.right , level+1);
+        traverse(node.left , level+1)
+    }
+    traverse(root , 0)
+    return result;
+};
