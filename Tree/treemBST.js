109c453ce6f6c8487324b6727bfd5f8930b853a0
Aman kohare <amankohare@gmail.com>
2023-08-21 17:19:18 +0530

    good

---
+++ Tree/treemBST.js
@@ -0,0 +1,12 @@
+var trimBST = function(root, low, high) {
+    if(!root) return null;
+    if(root.val < low){
+        return trimBST(root.right ,low , high )
+    }
+    if(root.val > high){
+        return trimBST(root.left , low,high)
+    }
+    root.left = trimBST(root.left , low, high)
+    root.right = trimBST(root.right , low, high)
+    return root
+};
