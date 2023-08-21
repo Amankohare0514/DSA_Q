75e3fc1b710f575752d3c487b0942dbb92266fca
Aman kohare <amankohare@gmail.com>
2023-08-21 17:22:38 +0530

    nice

---
+++ Tree/TreeToSTR.js
@@ -0,0 +1,11 @@
+var tree2str = function(root) {
+    if(!root) return "";
+    let result = root.val.toString();
+    if(root.left || root.right){
+        result += "(" + tree2str(root.left) + ")";
+    }
+    if(root.right){
+        result += "(" + tree2str(root.right) + ")"
+    }
+    return result
+};
