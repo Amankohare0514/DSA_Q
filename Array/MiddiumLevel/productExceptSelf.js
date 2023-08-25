48e9895e9b03269e98fb1a95b5a4d7ca7748e345
Aman kohare <amankohare@gmail.com>
2023-08-25 19:10:56 +0530

    hoy

---
+++ Array/MiddiumLevel/productExceptSelf.js
@@ -0,0 +1,16 @@
+var productExceptSelf = function(nums) {
+    let result = [];
+    let left = 1;
+    let right = 1;
+  
+    for (let i = 0; i < nums.length; i++) {
+      result[i] = left;
+      left *= nums[i];
+    }
+    for (let i = nums.length - 1; i >= 0; i--) {
+      result[i] *= right;
+      right *= nums[i];
+    }
+    return result;
+    
+  };
