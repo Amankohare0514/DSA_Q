d185c1aa912bb93d418e034e2c93947942ea94a7
Aman kohare <amankohare@gmail.com>
2023-09-07 10:45:31 +0530

    ho

---
+++ Array/MiddiumLevel/findMaxAverage.js
@@ -0,0 +1,13 @@
+var findMaxAverage = function(nums, k) {
+    let sum = 0;
+    let max = 0;
+    for(let i=0; i<k; i++){
+        sum += nums[i];
+    }
+    max = sum/k;
+    for(let i=k; i<nums.length; i++){
+        sum += nums[i] - nums[i-k];
+        max = Math.max(max , sum/k)
+    }
+    return max;
+};
