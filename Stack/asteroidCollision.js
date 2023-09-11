cb9fc6c0d64d93fbc612e0dc73355353642616e2
Aman kohare <amankohare@gmail.com>
2023-09-11 10:58:58 +0530

    f

---
+++ Stack/asteroidCollision.js
@@ -0,0 +1,12 @@
+var asteroidCollision = function(asteroids) {
+    let stack = [];
+    for(const a of asteroids){
+        if(a<0){
+            while(stack.at(-1)>0 && stack.at(-1)< -a) stack.pop()
+            if(stack.at(-1)+a == 0 && stack.pop()) continue
+            if(stack.at(-1)>0 && stack.at(-1) > -a) continue
+        }
+        stack.push(a)
+    }
+    return stack
+};
